d2163cedbe1834f392a99a8c693c89f5685d5a87
Johans08 <johans08lol@gmail.com>
2024-10-10 23:12:27 -0600

    -a envio de credenciales por correo -a descarga de credenciales -a marcas de tiempo

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,79 @@
+﻿using DataBase_RH_BanderaBlanca.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RH_BanderaBlanca.Controllers
+{
+    public class EmailController : Controller
+    {
+
+        private string correoCredencial = "johans08lol@gmail.com";
+        private string contrasenaCredencial = "hrko hhgq udbo rveu";
+        // Enviar Email
+        [HttpPost]
+        public ActionResult EnviarEmail(string image, string usuario, string contrasena)
+        {
+            string tempImagePath = Path.Combine(Path.GetTempPath(), "credenciales.jpg");
+
+            try
+            {
+                Persona _persona = new Persona();
+                string correoDestino = _persona.ObtenerCorreo(usuario);
+
+                // Decodificar la imagen de Base64
+                string base64Data = image.Substring("data:image/jpg;base64,".Length);
+                byte[] imageBytes = Convert.FromBase64String(base64Data);
+
+                // Guardar la imagen como archivo temporal
+                System.IO.File.WriteAllBytes(tempImagePath, imageBytes);
+
+                // Configurar el cliente SMTP con el servidor SMTP correcto de Gmail
+                var smtpClient = new SmtpClient("smtp.gmail.com")
+                {
+                    Port = 587, // Puerto para conexiones TLS/SSL
+                    Credentials = new NetworkCredential(correoCredencial, contrasenaCredencial), // Credenciales de Gmail
+                    EnableSsl = true, // Aseguramos la conexión con SSL
+                };
+
+                // Crear el mensaje
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress(correoCredencial),
+                    Subject = "Credenciales de Usuario",
+                    Body = $"Usuario: {usuario}<br/>Contraseña: {contrasena}",
+                    IsBodyHtml = true,
+                };
+
+                mailMessage.To.Add(correoDestino);
+
+                // Adjuntar la imagen usando `using` para liberar recursos
+                using (var attachment = new Attachment(tempImagePath))
+                {
+                    mailMessage.Attachments.Add(attachment);
+                    smtpClient.Send(mailMessage);
+                }
+
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+            finally
+            {
+                // Eliminar el archivo temporal después de usarlo
+                if (System.IO.File.Exists(tempImagePath))
+                {
+                    System.IO.File.Delete(tempImagePath);
+                }
+            }
+        }
+
+    }
+}
---
+++ Controllers/marcas_tiempoController.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataBase_RH_BanderaBlanca.Models;
+
+namespace RH_BanderaBlanca.Controllers
+{
+    public class marcas_tiempoController : Controller
+    {
+        private BD_BanderaBlancaEntities db = new BD_BanderaBlancaEntities();
+
+        [HttpPost]
+        public ActionResult ValidarMarca(int identificador)
+        {
+            try
+            {
+                int tipoMarca = 1;
+                DateTime hora = DateTime.Now;
+
+                Qr qr = new Qr();
+                bool ok = qr.ValidarQR(identificador);
+
+                if (ok)
+                {
+                    string diaHoy = ObtenerNombreDiaSemana(DateTime.Today);
+                    empleados _empleado = db.empleados.FirstOrDefault(e => e.Personas_Identificador.Equals(identificador));
+                    horarios _horario = db.horarios.FirstOrDefault(t => t.idEmpleado == _empleado.idEmpleado && t.dias.Nombre_Dia == diaHoy);
+
+                    marcas_tiempo _marcas_Tiempo = db.marcas_tiempo.FirstOrDefault(m => m.idEmpleado.Equals(_empleado.idEmpleado) && m.Fecha_Marca.Equals(DateTime.Today));
+
+                    if (_marcas_Tiempo == null)
+                    {
+                        tipoMarca = 2;
+                    }
+
+                    _marcas_Tiempo =  new marcas_tiempo
+                    { 
+                        Fecha_Marca = DateTime.Today,
+                        idCatalogo_Movimientos = tipoMarca,
+                        idEmpleado = _empleado.idEmpleado,
+                        Marca_Hora = DateTime.Now.TimeOfDay
+
+                    };
+
+                }
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
+
+        string ObtenerNombreDiaSemana(DateTime fecha)
+        {
+            switch (fecha.DayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    return "Domingo";
+                case DayOfWeek.Monday:
+                    return "Lunes";
+                case DayOfWeek.Tuesday:
+                    return "Martes";
+                case DayOfWeek.Wednesday:
+                    return "Miercoles";
+                case DayOfWeek.Thursday:
+                    return "Jueves";
+                case DayOfWeek.Friday:
+                    return "Viernes";
+                case DayOfWeek.Saturday:
+                    return "Sabado";
+                default:
+                    return string.Empty; // En caso de error o fecha inválida
+            }
+        }
+
+        // GET: marcas_tiempo
+        public ActionResult Index()
+        {
+            var marcas_tiempo = db.marcas_tiempo.Include(m => m.catalogo_movimientos);
+            return View(marcas_tiempo.ToList());
+        }
+
+        // GET: marcas_tiempo/Details/5
+        public ActionResult Details(DateTime id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            marcas_tiempo marcas_tiempo = db.marcas_tiempo.Find(id);
+            if (marcas_tiempo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(marcas_tiempo);
+        }
+
+        // GET: marcas_tiempo/Create
+        public ActionResult Create()
+        {
+            ViewBag.idCatalogo_Movimientos = new SelectList(db.catalogo_movimientos, "idCatalogo_Movimientos", "Descripcion_Movimientos");
+            return View();
+        }
+
+        // POST: marcas_tiempo/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Fecha_Marca,idEmpleado,idCatalogo_Movimientos,Marca_Hora,Tardia,Justificada")] marcas_tiempo marcas_tiempo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.marcas_tiempo.Add(marcas_tiempo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idCatalogo_Movimientos = new SelectList(db.catalogo_movimientos, "idCatalogo_Movimientos", "Descripcion_Movimientos", marcas_tiempo.idCatalogo_Movimientos);
+            return View(marcas_tiempo);
+        }
+
+        // GET: marcas_tiempo/Edit/5
+        public ActionResult Edit(DateTime id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            marcas_tiempo marcas_tiempo = db.marcas_tiempo.Find(id);
+            if (marcas_tiempo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idCatalogo_Movimientos = new SelectList(db.catalogo_movimientos, "idCatalogo_Movimientos", "Descripcion_Movimientos", marcas_tiempo.idCatalogo_Movimientos);
+            return View(marcas_tiempo);
+        }
+
+        // POST: marcas_tiempo/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Fecha_Marca,idEmpleado,idCatalogo_Movimientos,Marca_Hora,Tardia,Justificada")] marcas_tiempo marcas_tiempo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(marcas_tiempo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idCatalogo_Movimientos = new SelectList(db.catalogo_movimientos, "idCatalogo_Movimientos", "Descripcion_Movimientos", marcas_tiempo.idCatalogo_Movimientos);
+            return View(marcas_tiempo);
+        }
+
+        // GET: marcas_tiempo/Delete/5
+        public ActionResult Delete(DateTime id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            marcas_tiempo marcas_tiempo = db.marcas_tiempo.Find(id);
+            if (marcas_tiempo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(marcas_tiempo);
+        }
+
+        // POST: marcas_tiempo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(DateTime id)
+        {
+            marcas_tiempo marcas_tiempo = db.marcas_tiempo.Find(id);
+            db.marcas_tiempo.Remove(marcas_tiempo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
